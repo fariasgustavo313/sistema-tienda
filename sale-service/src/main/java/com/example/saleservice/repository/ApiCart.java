2963b6a205df670c4a1c9221878257f2a0d04e99
Gustavo Farias <fariasgustavo313@gmail.com>
2024-04-21 02:01:28 -0300

    implementando api gateway

--- sale-service/src/main/java/com/example/saleservice/repository/ApiCart.java
+++ sale-service/src/main/java/com/example/saleservice/repository/ApiCart.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "apicart", url = "http://localhost:8085/carts")
+@FeignClient(name = "apicart", url = "http://localhost:8086/carts")
 public interface ApiCart {
 
     @GetMapping("/{id_cart}")
